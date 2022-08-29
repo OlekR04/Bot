62d6876e65ea0acf31d1c8e63ca79bd68db8bb80
OlekR04 <102615020+OlekR04@users.noreply.github.com>
2022-08-29 22:54:57 +0200

    Add files via upload

---
+++ Supreme Bot/autoFill.js
@@ -0,0 +1,10 @@
+
+var email = "olekrenka997@gmail.com";
+var addres1 = "Miedziowa";
+var addres2 = "14/2";
+var city = "Radzionków";
+var postCode = "41-922";
+var creditCard = "1234 1234 1234 1234 1234";
+var fullName = "Olek Renka";
+document.getElementById("order_billing_name").value = fullName;
+document.getElementsByClassName("iCheck-helper")[1].click();
